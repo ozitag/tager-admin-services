8f8dadca4d5569ba0b16b006e662a0d821149414
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-07-09 11:36:54 +0300

    v0.1.1 - Fix i18n service initialization

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
 import { PluginFunction } from 'vue';
-import i18next from 'i18next';
+import i18next, { InitOptions, TFunction } from 'i18next';
 
 import configService from './configuration';
 
@@ -7,35 +7,48 @@ type I18nextResourceBundleArgs = Parameters<typeof i18next.addResourceBundle>;
 
 const resourceBundleArgs: Array<I18nextResourceBundleArgs> = [];
 
-export function addTranslations(...args: I18nextResourceBundleArgs) {
-  if (i18next.isInitialized) {
-    i18next.addResourceBundle(...args);
-  } else {
-    resourceBundleArgs.push(args);
-  }
-}
+const i18n = {
+  addTranslations(...args: I18nextResourceBundleArgs) {
+    if (i18next.isInitialized) {
+      i18next.addResourceBundle(...args);
+    } else {
+      resourceBundleArgs.push(args);
+    }
+  },
+  init(params: InitOptions): Promise<TFunction> {
+    const lang = configService.getConfig().LANGUAGE ?? 'en';
 
-export const TranslationPlugin: PluginFunction<any> = (VueConstructor) => {
-  const lang = configService.getConfig().LANGUAGE ?? 'en';
-
-  i18next.init(
-    {
-      lng: lang.toLowerCase(),
-      defaultNS: 'main',
-    },
-    (error, t) => {
-      if (error) {
-        console.error('TranslationPlugin Error: ', error);
-      } else {
+    return i18next
+      .init({
+        lng: lang.toLowerCase(),
+        defaultNS: 'main',
+        ...params,
+      })
+      .then(() => {
         if (resourceBundleArgs.length > 0) {
           resourceBundleArgs.forEach((args) => {
             i18next.addResourceBundle(...args);
           });
         }
 
-        VueConstructor.prototype.$t = t;
-        VueConstructor.$t = t;
+        return i18next.t;
+      });
+  },
+  getPlugin(): PluginFunction<any> {
+    return (VueConstructor) => {
+      if (!i18next.isInitialized) {
+        console.error(
+          "You're trying to get I18nPlugin, but i18n has not initialized yet"
+        );
+      } else {
+        const translate: TFunction = (...args: Parameters<TFunction>) =>
+          i18next.t(...args);
+
+        VueConstructor.prototype.$t = translate;
+        VueConstructor.$t = translate;
       }
-    }
-  );
-};
+    };
+  },
+} as const;
+
+export default i18n;
