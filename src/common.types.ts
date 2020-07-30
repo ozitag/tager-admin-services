f07676f6e64685d7efc1480b505ffd43f0a66ef0
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-07-30 11:55:17 +0300

    v0.2.1 - Add export of TranslateFunction type

--- src/common.types.ts
+++ src/common.types.ts
@@ -1,3 +1,5 @@
+import { TFunction } from 'i18next';
+
 export type ConstantMap<C extends string> = Readonly<Record<C, C>>;
 export type Nullable<T> = T | null;
 export type Nullish<T> = T | null | undefined;
@@ -79,3 +81,5 @@ export type FileType = {
   size: number;
   url: string;
 };
+
+export interface TranslateFunction extends TFunction {}
