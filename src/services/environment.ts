0359a5d82de93aa239ea0a210056248067310139
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-04-16 23:57:38 +0300

    v1.0.0-rc.28 - update env

--- src/services/environment.ts
+++ src/services/environment.ts
@@ -1,15 +1,27 @@
 let isInitialized = false;
 
 export interface AppEnvironment {
+  // VUE_APP_PUBLIC_PATH
   publicPath: string | undefined;
+  // VUE_APP_ENV
   appEnv: string | undefined;
+  // VUE_APP_API_URL
   apiUrl: string | undefined;
+  // VUE_APP_ACCESS_TOKEN
   accessToken: string | undefined;
+  // VUE_APP_WEBSITE_URL
   websiteUrl: string | undefined;
+  // VUE_APP_WEBSITE_BUTTON_URL
+  websiteButtonUrl: string | undefined;
+  // VUE_APP_SENTRY_DSN
   sentryDsn: string | undefined;
+  // VUE_APP_SENTRY_ENVIRONMENT
   sentryEnv: string | undefined;
+  // BASE_URL
   baseUrl: string | undefined;
+  // NODE_ENV
   nodeEnv: string | undefined;
+  // VUE_APP_VERSION
   version: string | undefined;
 }
 
@@ -21,6 +33,7 @@ export let environment = Object.freeze<AppEnvironment>({
   apiUrl: undefined,
   accessToken: undefined,
   websiteUrl: undefined,
+  websiteButtonUrl: undefined,
   sentryDsn: undefined,
   sentryEnv: undefined,
   version: undefined,
